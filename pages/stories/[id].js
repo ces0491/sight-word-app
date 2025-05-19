095b61e860a7aa82c760a22a800aa1852cb95998
ces0491 <cesairetobias@gmail.com>
2025-05-19 12:54:28 +0200

    import errors

--- pages/stories/[id].js
+++ pages/stories/[id].js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { useRouter } from 'next/router';
 import Head from 'next/head';
 import { useSession } from 'next-auth/react';
